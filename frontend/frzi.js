8931007cda7a2ac46acbb9a0a4b8701f595e5f22
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-06-16 20:11:09 +0530

    commits

---
+++ frontend/frzi.js
@@ -0,0 +1,6 @@
+0% (4/4), 46.29 KiB | 2.57 MiB/s, done.
+Total 4 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/faraz8725/zamin-hotel.git
+   2756ae8..79a2c45  main -> main
+PS C:\Users\DELL\Desktop\zamin-hotel2 - Copy>
